fc37779942fc15ef0dd91615e3af3b0ece43bede
biubiu-shen <1355322659@qq.com>
2022-07-13 11:49:49 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '人力资源后台管理',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * header 是否fixed定位
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 侧边栏主体图标是否显示
+   */
+  sidebarLogo: false
+}
